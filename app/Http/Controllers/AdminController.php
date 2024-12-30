688c8d4284331940f7cb24f8a8c1ac1fe531ec64
Ngọc Vân <nguyenngocvan.qng@gmail.com>
2024-12-30 10:09:33 +0700

    Admin Category Page

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Brand;
+use App\Models\Category;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
@@ -94,5 +95,10 @@ class AdminController extends Controller
         }
         $brand->delete();
         return redirect()->route('admin.brands')->with('status', 'Brand has been deleted successfully!');
+    }
+
+    public function categories() {
+        $categories = Category::orderBy('id','DESC')->paginate(10);
+        return view('admin.categories',compact('categories'));
     }
 }
